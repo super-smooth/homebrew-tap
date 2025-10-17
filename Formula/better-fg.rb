8d44d6a048e1ba562de884eed3b33364b029e624
Hugo Lewenhaupt <hugo.lewenhaupt@gmail.com>
2025-10-18 01:06:46 +0200

    Initial homebrew tap for better-fg

---
+++ Formula/better-fg.rb
@@ -0,0 +1,42 @@
+class BetterFg < Formula
+  desc "A more userfriendly and interactive version of fg"
+  homepage "https://github.com/super-smooth/better-fg"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/super-smooth/better-fg/releases/download/v1.0.0/better-fg_Darwin_arm64.tar.gz"
+      sha256 "sha256:527cc9a21c9d9f636512d2b7b5bd7f41806476e8b944dc2834ea550eeb655369"
+    else
+      url "https://github.com/super-smooth/better-fg/releases/download/v1.0.0/better-fg_Darwin_x86_64.tar.gz"
+      sha256 "sha256:030ba8533f90c547b3777ad18f6f75270b21240593b099a07960db22ca0c380d"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/super-smooth/better-fg/releases/download/v1.0.0/better-fg_Linux_arm64.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_ARM64_SHA"
+    else
+      url "https://github.com/super-smooth/better-fg/releases/download/v1.0.0/better-fg_Linux_x86_64.tar.gz"
+      sha256 "sha256:53002c3068a5bc525434936dee5fb832918b554d8bdb498c5956ede22e38a3dd"
+    end
+  end
+
+  def install
+    bin.install "better-fg"
+  end
+
+  def caveats
+    <<~EOS
+      To use better-fg, add this to your shell config (~/.bashrc, ~/.zshrc):
+        eval "$(better-fg init)"
+      
+      Then restart your shell and use the 'bfg' command.
+    EOS
+  end
+
+  test do
+    system "#{bin}/better-fg", "init"
+  end
+end
---
+++ README.md
@@ -0,0 +1,29 @@
+# super-smooth's Homebrew Tap
+
+This is the official Homebrew tap for CLI tools by super-smooth.
+
+## Available Formulas
+
+- **better-fg** - A more userfriendly and interactive version of fg
+
+## Installation
+
+```bash
+# Install better-fg
+brew install super-smooth/tap/better-fg
+
+# Or install any other available formula
+brew install super-smooth/tap/<formula-name>
+```
+
+## Usage
+
+### better-fg
+
+After installation, add this to your shell config:
+
+```bash
+eval "$(better-fg init)"
+```
+
+Then use the `bfg` command to interactively select background jobs.
